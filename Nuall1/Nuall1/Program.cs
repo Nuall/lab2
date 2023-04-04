570956b7371e855ba088287f8d7b934f35328aef
MelDmytro <dimaplay1504@gmail.com>
2023-04-04 16:15:56 +0300

    Nuall

---
+++ Nuall1/Nuall1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuall1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            Console.WriteLine("Введіть висоту");
+            x =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть основу");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("S = ");
+            Console.WriteLine(x * y);
+            Console.ReadLine();
+
+        }
+    }
+}
